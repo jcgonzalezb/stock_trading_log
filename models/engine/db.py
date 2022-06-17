af70efb5bdf88df3ce9a73b7be6335e556e9314c
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-17 09:30:53 -0500

    db updated

--- models/engine/db.py
+++ models/engine/db.py
@@ -1,2 +1,11 @@
-asdasdas
+from unicodedata import name
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
+# conection to the database
+app = Flask(__name__)
+uri = 'mysql+pymysql://root:@localhost:3306/stock_log_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
