41ca4148df0c0054200717efe1ae9ba8ca64a893
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-17 11:11:51 -0500

    colums restrictions updated

--- models/trade.py
+++ models/trade.py
@@ -8,19 +8,21 @@ class Trade(db.Model):
     """ Representation of trade """
     __tablename__ = 'trade'
 
-    trade_id = db.Column(db.String(60), primary_key=True)
-    user_id = db.Column(db.String(60), db.ForeignKey('user.user_id'))
-    trade_status = db.Column(db.String(60))
-    trade = db.Column(db.String(60))
-    company = db.Column(db.String(128))
-    ticker = db.Column(db.String(60))
-    number_employees = db.Column(db.Integer)
+    trade_id = db.Column(db.String(60), primary_key=True, nullable=False)
+    user_id = db.Column(db.String(60), db.ForeignKey('user.user_id'),
+                        nullable=False)
+    trade_status = db.Column(db.String(60), nullable=False)
+    trade = db.Column(db.String(60), nullable=False)
+    company = db.Column(db.String(128), nullable=False)
+    ticker = db.Column(db.String(60), nullable=False)
+    number_employees = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float)
-    trade_date = db.Column(db.DateTime)
+    trade_date = db.Column(db.DateTime(timezone=True))
 
     def __init__(self, trade_id, user_id, trade_status, trade, company,
                  ticker, number_employees, price, trade_date):
-
+        """ Python class constructor function job is to
+        initialize the instance of the class Trade """
         self.trade_id = trade_id
         self.user_id = user_id
         self.trade_status = trade_status
