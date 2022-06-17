2c1a2e3b9537c2bef143948e9de525296bcf45c6
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-17 10:52:41 -0500

    class created

--- models/trade.py
+++ models/trade.py
@@ -1,7 +1,32 @@
-from sqlite3 import adapt
-from django.conf import DEFAULT_HASHING_ALGORITHM_DEPRECATED_MSG
+""" Holds class Trade"""
+
+from flask_sqlalchemy import SQLAlchemy
+from models.db import db
 
 
-DEFAULT_HASHING_ALGORITHM_DEPRECATED_MSG
+class Trade(db.Model):
+    """ Representation of trade """
+    __tablename__ = 'trade'
 
-adapt
+    trade_id = db.Column(db.String(60), primary_key=True)
+    user_id = db.Column(db.String(60), db.ForeignKey('user.user_id'))
+    trade_status = db.Column(db.String(60))
+    trade = db.Column(db.String(60))
+    company = db.Column(db.String(128))
+    ticker = db.Column(db.String(60))
+    number_employees = db.Column(db.Integer)
+    price = db.Column(db.Float)
+    trade_date = db.Column(db.DateTime)
+
+    def __init__(self, trade_id, user_id, trade_status, trade, company,
+                 ticker, number_employees, price, trade_date):
+
+        self.trade_id = trade_id
+        self.user_id = user_id
+        self.trade_status = trade_status
+        self.trade = trade
+        self.company = company
+        self.ticker = ticker
+        self.number_employees = number_employees
+        self.price = price
+        self.trade_date = trade_date
