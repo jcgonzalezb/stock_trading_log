6759401e7fdb8c18bbf1fc436a61a7f24893d844
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-22 22:13:31 -0500

    user id fixed

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 """ Holds class User"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import INTEGER
 from config import db
 
 
@@ -8,7 +9,8 @@ class User(db.Model):
     """ Representation of user """
     __tablename__ = 'user'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(INTEGER(unsigned=True), primary_key=True,
+                   autoincrement=True, nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     session_id = db.Column(db.String(128))
