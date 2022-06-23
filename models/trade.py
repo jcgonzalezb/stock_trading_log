ce4dc6dfffa9d3cd6f1bbe4d07162c0d69e9f888
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-22 22:13:42 -0500

    trade id fixed

--- models/trade.py
+++ models/trade.py
@@ -1,6 +1,7 @@
 """ Holds class Trade"""
 
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import INTEGER
 from config import db
 
 
@@ -8,9 +9,10 @@ class Trade(db.Model):
     """ Representation of trade """
     __tablename__ = 'trade'
 
-    trade_id = db.Column(db.String(60), primary_key=True, nullable=False)
-    user_id = db.Column(db.String(60), db.ForeignKey('user.user_id'),
-                        nullable=False)
+    trade_id = db.Column(INTEGER(unsigned=True), primary_key=True,
+                         autoincrement=True, nullable=False)
+    user_id = db.Column(INTEGER(unsigned=True), db.ForeignKey(
+        'user.id'), nullable=False)
     trade_status = db.Column(db.String(60), nullable=False)
     trade = db.Column(db.String(60), nullable=False)
     company = db.Column(db.String(128), nullable=False)
@@ -18,17 +20,3 @@ class Trade(db.Model):
     quantity = db.Column(db.Integer, nullable=False)
     price = db.Column(db.Float)
     trade_date = db.Column(db.DateTime(timezone=True))
-
-    def __init__(self, trade_id, user_id, trade_status, trade, company,
-                 ticker, quantity, price, trade_date):
-        """ Python class constructor function job is to
-        initialize the instance of the class Trade """
-        self.trade_id = trade_id
-        self.user_id = user_id
-        self.trade_status = trade_status
-        self.trade = trade
-        self.company = company
-        self.ticker = ticker
-        self.quantity = quantity
-        self.price = price
-        self.trade_date = trade_date
