296dd4259a24a7fb3c995babb73a41552182280a
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-25 15:36:40 -0500

    secret key

--- config.py
+++ config.py
@@ -7,5 +7,6 @@ app = Flask(__name__)
 uri = 'mysql+pymysql://root:@localhost:3306/stock_log_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = "fe620408-4b11-498b-9757-1d47e22b2199"
 
 db = SQLAlchemy(app)
