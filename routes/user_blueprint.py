61e0fe16a58fbbd7b448c6c26c19874fbf56a3b1
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-25 15:36:18 -0500

    testing

--- routes/user_blueprint.py
+++ routes/user_blueprint.py
@@ -18,28 +18,16 @@ def get_user_profile():
         return jsonify(user_schemas.dump(user)), 200
     except Exception as e:
         abort(403)
-    
+
 @user_blueprint.route('/', methods=['POST'], strict_slashes=False)
 def create_user():
     """ Create a user in the database """
     data = request.get_json()
     email = data.get('email', None)
     password = data.get('password', None)
-    non_empty = Validator.validate_nonempty(email, password)
-
-    if non_empty:
-
-        try:
-            Password.validate_password(password)
-        except PasswordNotValidError as e:
-            return {'message': f'sorry, {e}'}, 400
-
-        hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-        password = hashed_password.decode()
-        new_user = User(email=email, password=password)
-        db.session.add(new_user)
-        db.session.commit()
-        return jsonify({'message': 'New user created!'})
-
-    else:
-        return jsonify({'message': 'Some parameters are missing'}), 400
+    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    password = hashed_password.decode()
+    new_user = User(email=email, password=password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'New user created!'})
