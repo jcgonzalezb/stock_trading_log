3ff2f67482ece7aa3912da34c6d340f7d1bd1e0b
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 17:19:38 -0500

    file updated

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
-from json.tool import main
-from crypt import methods
-from unicodedata import name
-from flask import Flask, jsonify, request
-from routes.user_blueprint import user_blueprint
-from routes.trade_blueprint import trade_blueprint
+from flask_jwt import JWT
+from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.exc import NoResultFound
-from flask_marshmallow import Marshmallow
+
 from config import app, db
+
 from models.user import User
 from models.trade import Trade
+from security.security import authenticate, identity
+
+
+from routes.user_blueprint import user_blueprint
+from routes.trade_blueprint import trade_blueprint
+
 from schemas.ma import ma
 from schemas.trade_schema import TradeSchema
 from schemas.user_schema import UserSchema
-
-
 
 # Create the tables that are associated with the models.
 db.create_all()
@@ -27,19 +27,24 @@ trade_schema = TradeSchema()
 user_schemas = UserSchema(many=True)
 trade_schemas = TradeSchema(many=True)
 
+jwt = JWT(app, authenticate, identity)
+
 app.register_blueprint(user_blueprint)
 app.register_blueprint(trade_blueprint)
+
 
 @app.route('/')
 def index():
     """ Index page. No token needed """
     return jsonify({'message': 'Welcome to the stock trading log!'})
 
+
 @app.route('/all_users', methods=['GET'])
 def get_users_info():
     """ User information stored in the database """
     results = User.query.all()
     return jsonify(user_schemas.dump(results))
+
 
 @app.route('/unprotected')
 def unprotected():
@@ -52,6 +57,7 @@ def get_all_trades():
     """ Show all trades """
     results = Trade.query.all()
     return jsonify(trade_schemas.dump(results))
+
 
 @app.route('/update/<trade_id>', methods=['PUT'])
 def update_trade(trade_id):
@@ -72,6 +78,7 @@ def update_trade(trade_id):
     trade.trade_date = data['trade_date']
     db.session.commit()
     return jsonify({'message': 'The trade has been updated!'})
+
 
 if __name__ == "__main__":
     app.run(debug=True)
