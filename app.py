a94396099c40fb59217153ff11361a152706fe31
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 22:38:30 -0500

    testing fixed

--- app.py
+++ app.py
@@ -7,6 +7,11 @@ from flask_jwt_extended import JWTManager
 from config import app, db
 from api.routes import create_routes
 
+from models.user import User
+from schemas.user_schema import UserSchema
+user_schema = UserSchema()
+user_schemas = UserSchema(many=True)
+
 # init api and routes
 api = Api(app=app)
 create_routes(api=api)
@@ -19,6 +24,13 @@ db.create_all()
 def index():
     """ Index page. No token needed """
     return jsonify({'message': 'Welcome to the stock trading log!'})
+
+@app.route('/all_users', methods=['GET'])
+def get_users_info():
+    """ User information stored in the database """
+    results = User.query.all()
+    return jsonify(user_schemas.dump(results))
+
 
 # init jwt manager
 jwt = JWTManager(app=app)
