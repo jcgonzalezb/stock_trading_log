3dbfcea625d5db87468b6163ea0c15000e0cdc12
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 20:53:00 -0500

    query fixed

--- api/trade.py
+++ api/trade.py
@@ -2,10 +2,15 @@ from flask import jsonify
 from flask_restful import Resource
 
 from models.trade import Trade
+from schemas.ma import ma
+from schemas.trade_schema import TradeSchema
+
+trade_schema = TradeSchema()
+trade_schemas = TradeSchema(many=True)
+
 
 class TradeApi(Resource):
     def get(self):
-        output = Trade.query.all()
-        print(type(output))
-        print(output.to_dict())
-        return jsonify({'result': output.to_dict()})
+        """ Show all trades """
+        results = Trade.query.all()
+        return jsonify(trade_schemas.dump(results))
