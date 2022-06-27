2b14b875ad6b88e82034ed6afbf9843dd639a50f
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 23:28:12 -0500

    user fixed

--- models/user.py
+++ models/user.py
@@ -14,18 +14,3 @@ class User(db.Model):
     password = db.Column(db.String(128), nullable=False)
     session_id = db.Column(db.String(128))
 
-    def generate_pw_hash(self):
-        self.password = generate_password_hash(password=self.password).decode('utf-8')
-    # Use documentation from BCrypt for password hashing
-    generate_pw_hash.__doc__ = generate_password_hash.__doc__
-
-    def check_pw_hash(self, password: str) -> bool:
-        return check_password_hash(pw_hash=self.password, password=password)
-    # Use documentation from BCrypt for password hashing
-    check_pw_hash.__doc__ = check_password_hash.__doc__
-
-    def save(self, *args, **kwargs):
-        # Overwrite Document save method to generate password hash prior to saving
-        if self._created:
-            self.generate_pw_hash()
-        super(User, self).save(*args, **kwargs)
