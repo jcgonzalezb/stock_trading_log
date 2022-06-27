7c7726a9ac70c17c63bf6c4d9f1982923911de5f
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 19:41:51 -0500

    fixed

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import datetime
-from unicodedata import name
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -11,7 +11,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # JWT configuration
 app.config['SECRET_KEY'] = "fe620408-4b11-498b-9757-1d47e22b2199"
-app.config['JWT_AUTH_USERNAME_KEY'] = "email"
 app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(minutes=20)
 
 # Create a database object using the SQLAlchemy class,
