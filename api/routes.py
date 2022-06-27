f3af997c7b66a8113ad65bf104d2805bce2fba8d
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 21:44:11 -0500

    new routes

--- api/routes.py
+++ api/routes.py
@@ -2,11 +2,15 @@
 from flask_restful import Api
 
 # project resources
+from api.authentication import SignUpApi, LoginApi
 from api.user import UsersApi
 from api.trade import TradesApi
 
 
 def create_routes(api):
+
+    api.add_resource(SignUpApi, '/authentication/signup/')
+    api.add_resource(LoginApi, '/authentication/login/')
 
     api.add_resource(UsersApi, '/user/')
 
