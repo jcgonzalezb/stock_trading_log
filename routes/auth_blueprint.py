50d221a175c90a99440fd758b0c8d84d51ba7fef
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-27 14:43:30 -0500

    file updated

---
+++ routes/auth_blueprint.py
@@ -0,0 +1,62 @@
+# flask packages
+from flask import Blueprint, Response, request, jsonify, make_response
+from config import db
+from models.user import User
+from schemas.user_schema import UserSchema
+from sqlalchemy.exc import NoResultFound
+
+# project resources
+from config import db, app
+from models.user import User
+
+# external packages
+from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+import datetime
+
+
+auth_blueprint = Blueprint('auth_blueprint', __name__, url_prefix='/')
+user_schema = UserSchema()
+user_schemas = UserSchema(many=True)
+
+
+@auth_blueprint.route('/signup', methods=['GET'], strict_slashes=False)
+@staticmethod
+def register_user() -> Response:
+    """
+    POST response method for creating user.
+    :return: JSON object
+    """
+    data = request.get_json()
+    email = data.get('email', None)
+    password = data.get('password', None)
+    hashed_password = generate_password_hash(password, method='sha256')
+    new_user = User(email=email, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'message': 'New user created!'})
+
+@auth_blueprint.route('/login', methods=['POST'], strict_slashes=False)
+@staticmethod
+def login() -> Response:
+    """
+    POST response method for retrieving user web token.
+    :return: JSON object
+    """
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(email=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'email': user.email, 'exp': datetime.datetime.utcnow(
+        ) + datetime.timedelta(minutes=60)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token': token})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
--- routes/user_blueprint.py
+++ routes/user_blueprint.py
@@ -1,36 +1,27 @@
-from flask_jwt import jwt_required
-from flask import Blueprint, request, jsonify, abort
+# flask packages
+from flask import Response, request, jsonify, Blueprint, abort
 from config import db
 from models.user import User
-from validators.validator import Validator
-from validators.password_validator import Password, PasswordNotValidError
 from schemas.user_schema import UserSchema
+from sqlalchemy.exc import NoResultFound
 
-import bcrypt
+#The token verification script
+from security.authenticate import token_required
 
-user_blueprint = Blueprint('user_blueprint', __name__, url_prefix='/users')
+user_blueprint = Blueprint('user_blueprint', __name__, url_prefix='/user')
+user_schema = UserSchema()
 user_schemas = UserSchema(many=True)
 
-@user_blueprint.route('/', methods=['GET'], strict_slashes=False)
-@jwt_required()
-def get_user_profile():
-    """ User information stored in the database """
-    user_id = request.id
+@user_blueprint.route('/<user_id>', methods=['GET'], strict_slashes=False)
+@token_required()
+def get_user_profile(current_user, request, user_id: str) -> Response:
+    """
+    GET response method for acquiring single user data.
+    JSON Web Token is required.
+    :return: JSON object
+    """
     try:
-        user = User.query.filter_by(id=user_id).first()
-        return jsonify(user_schemas.dump(user)), 200
-    except Exception as e:
-        abort(403)
-
-@user_blueprint.route('/', methods=['POST'], strict_slashes=False)
-def create_user():
-    """ Create a user in the database """
-    data = request.get_json()
-    email = data.get('email', None)
-    password = data.get('password', None)
-    hashed_password = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
-    password = hashed_password.decode()
-    new_user = User(email=email, password=password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'message': 'New user created!'})
+        result = User.query.filter_by(id=user_id).one()
+    except NoResultFound:
+        return {"message": "User could not be found."}, 400
+    return user_schema.jsonify(result)
