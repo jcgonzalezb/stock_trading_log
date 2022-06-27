a44314569d805e9c85dece1904068ddf4368c021
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 22:40:58 -0500

    config updated

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,6 +9,10 @@ app = Flask(__name__)
 uri = 'mysql+pymysql://root:@localhost:3306/stock_log_db'
 app.config['SQLALCHEMY_DATABASE_URI'] = uri
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# JWT configuration
+app.config['SECRET_KEY'] = "fe620408-4b11-498b-9757-1d47e22b2199"
+app.config['JWT_EXPIRATION_DELTA'] = datetime.timedelta(minutes=20)
 
 # Create a database object using the SQLAlchemy class,
 # passing the application instance to connect the Flask
