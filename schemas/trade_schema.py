338288a3ffdb9e18cf95ee90de7a18453ee54ec4
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-26 20:46:08 -0500

    schema files

---
+++ schemas/trade_schema.py
@@ -0,0 +1,8 @@
+from flask_marshmallow import Marshmallow
+from schemas.ma import ma
+
+
+class TradeSchema(ma.Schema):
+    class Meta:
+        fields = ("trade_id", "user_id", "trade_status", "trade", "company",
+                  "ticker", "quantity", "price", "trade_date")
---
+++ schemas/user_schema.py
@@ -0,0 +1,7 @@
+from flask_marshmallow import Marshmallow
+from schemas.ma import ma
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "email", "password", "session_id")
