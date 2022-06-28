64ea765f4f47c79c20a7eb28315b703fed76fdea
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-28 14:01:19 -0500

    syntax fixed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 
-#The token verification script
+# The token verification script
 from security.authenticate import token_required
 
 # local packages
@@ -31,27 +31,46 @@ app.register_blueprint(auth_blueprint)
 app.register_blueprint(user_blueprint)
 app.register_blueprint(trade_blueprint)
 
+
 @app.route('/')
 def index():
-    """ Index page. No token needed """
+    """
+    Index page. No token needed
+    :return: JSON object
+    """
     return jsonify({'message': 'Welcome to the stock trading log!'})
 
-#Testing user access. No token needed
+
 @app.route('/unprotected')
 def unprotected():
-    return jsonify({'message' : 'Anyone can view this!'})
+    """
+    Testing user access. No token needed
+    :return: JSON object
+    """
+    return jsonify({'message': 'Anyone can view this!'})
 
-#Testing user access. Token needed
+
 @app.route('/protected')
 @token_required
 def protected(current_user):
-    return jsonify({'message' : 'This is on available for people with valid tokens.'})
+    """
+    Testing user access.
+    JSON Web Token is required.
+    :return: JSON object
+    """
+    return jsonify({'message':
+                    'This section is available for people with valid tokens.'})
+
 
 @app.route('/all_users', methods=['GET'])
 def get_users_info():
-    """ User information stored in the database """
+    """
+    GET response method for acquiring all users data.
+    :return: JSON object
+    """
     results = User.query.all()
     return jsonify(user_schemas.dump(results))
+
 
 if __name__ == '__main__':
     # Main entry point when run in stand-alone mode.
