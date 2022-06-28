51ec5881854bdb946fbd3f3df954c01607a77931
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-28 18:00:25 -0500

    errors updated

--- validators/errors.py
+++ validators/errors.py
@@ -2,27 +2,37 @@
 from flask import Response, jsonify
 
 
-def unauthorized() -> Response:
-    output = {"error":
-              {"msg": "401 error: The email or password provided is invalid."}
-              }
-    resp = jsonify({'result': output})
-    resp.status_code = 401
+def trade_not_found() -> Response:
+    msg1 = "Trade could not be found."
+    msg2 = "This route is currently not supported."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 404
     return resp
 
 
 def forbidden() -> Response:
-    output = {"error":
-              {"msg": "403 error: The current user is not authorized to take this action."}
-              }
-    resp = jsonify({'result': output})
+    msg1 = "The trade_id or user_id or trade_status values cannot be updated."
+    msg2 = "The current user is not authorized to take this action."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 403
+    return resp
+
+
+def forbidden_status() -> Response:
+    msg1 = "The trade_status cannot be changed."
+    msg2 = "The trade_status is currently in disable."
+    msg3 = "The current user is not authorized to take this action."
+    output = {"message": msg1 + msg2 + msg3}
+    resp = jsonify(output)
     resp.status_code = 403
     return resp
 
 
 def invalid_route() -> Response:
     output = {"error":
-              {"msg": "404 error: This route is currently not supported. See API documentation."}
+              {"msg": "404 error: This route is currently not supported."}
               }
     resp = jsonify({'result': output})
     resp.status_code = 404
