f63ecf1ae29ac73b508a5c6cee53864ecd25df74
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-27 22:35:59 -0500

    profile updated

--- routes/user_blueprint.py
+++ routes/user_blueprint.py
@@ -8,7 +8,6 @@ from sqlalchemy.exc import NoResultFound
 #The token verification script
 from security.authenticate import token_required
 import jwt
-import requests
 
 user_blueprint = Blueprint('user_blueprint', __name__, url_prefix='/user')
 user_schema = UserSchema()
@@ -22,15 +21,8 @@ def get_user_profile(current_user) -> Response:
     JSON Web Token is required.
     :return: JSON object
     """
-    url = "http://127.0.0.1:5000/user"
-    response = requests.get(url)
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNjU2Mzg3ODg2fQ.4KzCyUs7IEsMPdlw2CZfbRpjFYEGoA5lFwIC89hiKpw"
-
-    decoded = jwt.decode(token, options={"verify_signature": False}) # works in PyJWT >= v2.0
-    print (decoded)
-    print (decoded["id"])
-    try:
-        result = User.query.filter_by(id=decoded["id"]).one()
-    except NoResultFound:
-        return {"message": "User could not be found."}, 400
+    head = request.headers
+    token = head['token']
+    decoded = jwt.decode(token, options={"verify_signature": False})
+    result = User.query.filter_by(id=decoded["id"]).one()
     return user_schema.jsonify(result), 200
