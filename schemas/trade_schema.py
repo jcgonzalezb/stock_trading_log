d7d5de6566b930a93eace2f123eb5fd799827799
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-28 13:24:29 -0500

    syntax fixed

--- schemas/trade_schema.py
+++ schemas/trade_schema.py
@@ -1,6 +1,13 @@
+# local package
 from schemas.ma import ma
 
+
 class TradeSchema(ma.Schema):
+    """
+    Trade Marshmallow Schema
+    Marshmallow schema used for loading/dumping Trades
+    """
     class Meta:
+        """ Fields to expose """
         fields = ("trade_id", "user_id", "trade_status", "trade", "company",
                   "ticker", "quantity", "price", "trade_date")
--- schemas/user_schema.py
+++ schemas/user_schema.py
@@ -1,7 +1,12 @@
-from flask_marshmallow import Marshmallow
+# local package
 from schemas.ma import ma
 
 
 class UserSchema(ma.Schema):
+    """
+    User Marshmallow Schema
+    Marshmallow schema used for loading Users
+    """
     class Meta:
+        """ Fields to expose """
         fields = ("id", "email", "password", "name")
