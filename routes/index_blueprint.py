ea02fe32302ac92e04b08e1cc15a3dc31e01b7f8
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-29 14:01:20 -0500

    endpoints updated

---
+++ routes/index_blueprint.py
@@ -0,0 +1,58 @@
+# flask packages
+from security.authenticate import token_required
+from flask import Blueprint, jsonify
+
+# project resources
+from config import app, db
+from models.user import User
+from schemas.user_schema import UserSchema
+
+# One response
+user_schema = UserSchema()
+
+# Many responses
+user_schemas = UserSchema(many=True)
+
+# The token verification script
+
+index_blueprint = Blueprint('index_blueprint', __name__, url_prefix='/')
+
+
+@app.route('/')
+def index():
+    """
+    Index page. No token needed
+    :return: JSON object
+    """
+    return jsonify({'message': 'Welcome to the stock trading log!'})
+
+
+@app.route('/unprotected')
+def unprotected():
+    """
+    Testing user access. No token needed
+    :return: JSON object
+    """
+    return jsonify({'message': 'Anyone can view this!'})
+
+
+@app.route('/protected')
+@token_required
+def protected(current_user):
+    """
+    Testing user access.
+    JSON Web Token is required.
+    :return: JSON object
+    """
+    return jsonify({'message':
+                    'This section is available for people with valid tokens.'})
+
+
+@app.route('/all_users', methods=['GET'])
+def get_users_info():
+    """
+    GET response method for acquiring all users data.
+    :return: JSON object
+    """
+    results = User.query.all()
+    return jsonify(user_schemas.dump(results))
