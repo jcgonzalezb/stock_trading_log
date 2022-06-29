19cf338d2883c63490f4ac1f362cf22ef31ae846
jcgonzalezb <jcgonzalez@gmail.com>
2022-06-28 20:08:54 -0500

    errors syntax fixed

--- validators/errors.py
+++ validators/errors.py
@@ -2,6 +2,33 @@
 from flask import Response, jsonify
 
 
+def empty_data() -> Response:
+    msg1 = "The JSON sent is empty."
+    msg2 = "The information sent is not acceptable."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 406
+    return resp
+
+
+def forbidden_new_user() -> Response:
+    msg1 = "The id cannot be inserted."
+    msg2 = "The current user is not authorized to take this action."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 403
+    return resp
+
+
+def forbidden_update_user() -> Response:
+    msg1 = "The id, email or password cannot be updated."
+    msg2 = "The current user is not authorized to take this action."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 403
+    return resp
+
+
 def trade_not_found() -> Response:
     msg1 = "Trade could not be found."
     msg2 = "This route is currently not supported."
@@ -12,7 +39,7 @@ def trade_not_found() -> Response:
 
 
 def forbidden() -> Response:
-    msg1 = "The trade_id or user_id or trade_status values cannot be updated."
+    msg1 = "The trade_id, user_id or trade_status values cannot be updated."
     msg2 = "The current user is not authorized to take this action."
     output = {"message": msg1 + msg2}
     resp = jsonify(output)
@@ -20,7 +47,7 @@ def forbidden() -> Response:
     return resp
 
 
-def forbidden_new() -> Response:
+def forbidden_new_trade() -> Response:
     msg1 = "The trade_id or user_id cannot be inserted."
     msg2 = "The current user is not authorized to take this action."
     output = {"message": msg1 + msg2}
@@ -39,10 +66,10 @@ def forbidden_status() -> Response:
     return resp
 
 
-def invalid_route() -> Response:
-    output = {"error":
-              {"msg": "404 error: This route is currently not supported."}
-              }
-    resp = jsonify({'result': output})
-    resp.status_code = 404
+def forbidden_register() -> Response:
+    msg1 = "The user already exist."
+    msg2 = "The current user is not authorized to take this action."
+    output = {"message": msg1 + msg2}
+    resp = jsonify(output)
+    resp.status_code = 403
     return resp
