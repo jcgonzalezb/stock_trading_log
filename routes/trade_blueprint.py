388366d903967ded21e298874067cc20988d7ee6
jcgonzalezb <jcgonzalez@gmail.com>
2022-07-05 18:40:31 -0500

    trade list

--- routes/trade_blueprint.py
+++ routes/trade_blueprint.py
@@ -1,5 +1,5 @@
 # flask packages
-from flask import Response, Blueprint, request, jsonify
+from flask import Response, Blueprint, request, jsonify, render_template
 from sqlalchemy.exc import NoResultFound
 
 # project resources
@@ -78,7 +78,8 @@ def profile_trade(current_user, trade_id: str) -> Response:
             return forbidden_trade()
     except NoResultFound:
         return trade_not_found()
-    return trade_schema.jsonify(trade)
+    return render_template('trade_profile.html', trade=trade)
+    #return trade_schema.jsonify(trade)
 
 
 @trade_blueprint.route('/update_status/<trade_id>',
